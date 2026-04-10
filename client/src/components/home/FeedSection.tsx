27d11164beb1da598a518eceaee7e3b677961c9c
ani5hpdl <178013051+ani5hpdl@users.noreply.github.com>
2026-04-10 21:35:43 +0545

    feat: show posts as clickable cards that push to detail view

--- client/src/components/home/FeedSection.tsx
+++ client/src/components/home/FeedSection.tsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import type { Post } from "../../api/posts.api";
 import { PostCard } from "../posts/PostCard";
+import { useNavigate } from "react-router-dom";
 
 interface FeedSectionProps {
   posts: Post[] | undefined;
 }
 
 export function FeedSection({ posts }: FeedSectionProps) {
+
+  const navigate = useNavigate();
+  const handlePostClick = (id: string) => {
+    navigate(`/post/${id}`)
+  }
+
   if (!posts) return null;
 
   return (
     <div className="max-w-3xl mx-auto py-8 px-6">
-      <PostCard data={posts} />
+      {posts && posts.map((post)=>(
+        <PostCard post={post} onClick={handlePostClick}/>
+      ))}
     </div>
   );
 }
